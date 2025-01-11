b49d0d5a252a24b9689a46c7bc48d7e2c6252984
안소희 <2022112701@dgu.ac.kr>
2025-01-11 13:35:29 +0900

    환경 설정

---
+++ bucketNote/src/test/java/com/bucketNote/bucketNote/BucketNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bucketNote.bucketNote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BucketNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
